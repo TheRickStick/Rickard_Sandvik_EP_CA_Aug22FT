3347ba20eab668117c04c037468937e7d30f1105
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-06-05 21:49:01 +0200

    blabla

--- routes/index.js
+++ routes/index.js
@@ -74,17 +74,70 @@ router.get('/orders', authenticateToken, async (req, res) => {
     const orders = await db.Order.findAll({
       where: { UserId: user.id },
       include: [
-        { model: db.User, attributes: ['firstName', 'lastName'] },
-        { model: db.OrderItem, include: { model: db.Item } },
+        {
+          model: db.User,
+          attributes: ['firstName', 'lastName', 'username'],
+        },
+        {
+          model: db.OrderItem,
+          attributes: ['id', 'quantity'],
+          include: {
+            model: db.Item,
+            attributes: ['id', 'sku', 'name', 'price', 'stock', 'img_url'],
+            include: {
+              model: db.Category,
+              attributes: ['id', 'name'],
+            },
+          },
+        },
       ],
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
     });
 
-    res.json(orders);
+    const processedOrders = orders.map((order) => {
+      if (order.status === 'Completed') {
+        return {
+          id: order.id,
+          status: order.status,
+          User: {
+            firstName: order.User.firstName,
+            lastName: order.User.lastName,
+          },
+          OrderItems: order.OrderItems.map((orderItem) => {
+            return {
+              id: orderItem.id,
+              quantity: orderItem.quantity,
+              Item: {
+                id: orderItem.Item.id,
+                sku: orderItem.Item.sku,
+                name: orderItem.Item.name,
+                price: orderItem.Item.price,
+                stock: orderItem.Item.stock,
+                img_url: orderItem.Item.img_url,
+                Category: {
+                  id: orderItem.Item.Category.id,
+                  name: orderItem.Item.Category.name,
+                },
+              },
+            };
+          }),
+        };
+      } else {
+        return {
+          id: order.id,
+          status: order.status,
+        };
+      }
+    });
+
+    res.json(processedOrders);
   } catch (err) {
     console.log('Error:', err);
     res.status(500).json({ message: err.message });
   }
 });
+
+
 
 // Fetch all orders for the admin user
 router.get('/allorders', authenticateToken, async (req, res) => {
